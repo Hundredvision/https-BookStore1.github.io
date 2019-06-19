1b0c37eb476f6b34a70a27acf4992360b5306ac5
Hundredvision <https://HundredVision.github.io/>
2019-06-19 22:17:59 +0530

    MOST POPULAR BOOKS OF WEEK
    MOST POPULAR BOOKS OF WEEK

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    $('.hamburger-menu').click(function() {
+       $('.navigation').toggleClass('change');
+    });
+
+
+    lightbox.option({
+        'wrapAround': true
+    });
+
+    $(window).scroll(function() {
+        let position = $(this).scrollTop();
+        if(position >= 350) {
+            $('.gallery').addClass('change');
+        } else {
+            $('.gallery').removeClass('change');
+        }
+    }); 
+});
+
+
+
+
+
+
+   
+
+
+
+
